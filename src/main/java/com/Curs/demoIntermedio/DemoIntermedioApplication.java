eb252a238c36965cf0f5a0872549df8dfca208e3
carjox <carlosbm1982@yahoo.es>
2023-04-26 16:56:54 -0500

    empezando Intermedio

---
+++ src/main/java/com/Curs/demoIntermedio/DemoIntermedioApplication.java
@@ -0,0 +1,13 @@
+package com.Curs.demoIntermedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIntermedioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoIntermedioApplication.class, args);
+    }
+
+}
